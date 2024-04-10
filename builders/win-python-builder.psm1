fa4316e91ba6cda881971d2e2877ef6e0bed1344
HarithaVattikuti <73516759+HarithaVattikuti@users.noreply.github.com>
2024-04-10 14:53:32 -0500

    added arm64 win change

--- builders/win-python-builder.psm1
+++ builders/win-python-builder.psm1
@@ -61,6 +61,9 @@ class WinPythonBuilder : PythonBuilder {
                 $ArchitectureExtension = ".amd64"
             }
         }
+        elseif ($this.Architecture -eq "arm64") {
+            $ArchitectureExtension = "-arm64"
+        }
 
         return $ArchitectureExtension
     }
